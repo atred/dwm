c5602254e7511eb126f0e9607e2476a3e58f9b85
Andrew Redinbo <atredinbo@gmail.com>
2020-12-08 03:45:28 -0500

    fixed font issues

--- config.def.h
+++ config.def.h
@@ -5,14 +5,14 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Fira Code Medium:size=11" };
+static const char dmenufont[]       = "Fira Code Medium:size=11";
 static const char col_stafg[]       = "#ebdbb2";
 static const char col_stabg[]       = "#282828";
-static const char col_stabo[]       = "#504945";
-static const char col_altfg[]       = "#282828";
-static const char col_altbg[]       = "#a89984";
-static const char col_altbo[]       = "#a89984";
+static const char col_stabo[]       = "#3c3836";
+static const char col_altfg[]       = "#3c3836";
+static const char col_altbg[]       = "#bdae93";
+static const char col_altbo[]       = "#504945";
 static const char *colors[][3]      = {
 	/*               fg           bg         border   */
 	[SchemeNorm]   = { col_stafg, col_stabg, col_stabo },
